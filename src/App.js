38858e20b10a27427f20866c9cff7194e775e3c1
Sebastián Aedo <sebastian.aedo29@gmail.com>
2019-11-20 00:35:06 -0300

    added material ui, first form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import TextField from '@material-ui/core/TextField'
+import { makeStyles } from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
 import logo from './logo.svg';
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  }
+})
+
+function Table() {
+
+}
+
 function App() {
+
+  const [requirement, setRequirement] = useState(0)
+  
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-body">
+
+          <div className="App-content">
+          
+          <div className='input-box'>
+            <TextField
+              label='Exigencia'
+              onChange={(v) => setRequirement(v.target.value)}
+              type="number"
+              fullWidth={false}
+              value={requirement}
+              InputProps={{endAdornment: "%"}}
+            />
+          </div>
+
+
+        </div>
+
+      </div>
     </div>
+    </ThemeProvider>
   );
 }
 
