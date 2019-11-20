144a806f6c3d8c43a11f09529130944a1a088082
Sebastián Aedo <sebastian.aedo29@gmail.com>
2019-11-20 01:08:33 -0300

    added automatic form creation

--- src/App.js
+++ src/App.js
@@ -12,13 +12,42 @@ const theme = createMuiTheme({
   }
 })
 
-function Table() {
+function ParameterInput(props) {
 
+  const { keyName, values, handler } = props;
+  const { name, value } = values;
+  
+  return (
+    <div className='input-box'>
+      <TextField
+      label={name}
+      onChange={(v) => handler(keyName, v.target.value)}
+      type="number"
+      fullWidth={false}
+      value={value}
+      InputProps={keyName == 'exigencia' ? {endAdornment: "%"} : {}}
+      error={isNaN(value) || value < 0 }
+    />
+  </div>
+  )
 }
 
 function App() {
 
-  const [requirement, setRequirement] = useState(0)
+  const [parameters, setParameters] = useState({
+    'exigencia': {name: "Exigencia", value: 60},
+    'notaMin': {name: "Nota mínima", value: 2.0},
+    'notaMax': {name: "Nota máxima", value: 7.0},
+    'puntajeMin': {name: "Puntaje mínimo", value: 0},
+    'puntajeMax': {name: "Puntaje máximo", value: 100},
+    'incremento': {name: "Incremento", value: 1}
+  });
+
+  const changeValue = (key, value) => {
+    const _p = JSON.parse(JSON.stringify(parameters));
+    _p[key].value = value;
+    setParameters(_p);
+  }
   
   return (
     <ThemeProvider theme={theme}>
@@ -27,16 +56,8 @@ function App() {
 
           <div className="App-content">
           
-          <div className='input-box'>
-            <TextField
-              label='Exigencia'
-              onChange={(v) => setRequirement(v.target.value)}
-              type="number"
-              fullWidth={false}
-              value={requirement}
-              InputProps={{endAdornment: "%"}}
-            />
-          </div>
+          
+          {Object.keys(parameters).map(v => (<ParameterInput key={v} keyName={v} values={parameters[v]} handler={changeValue}/>))}
 
 
         </div>
