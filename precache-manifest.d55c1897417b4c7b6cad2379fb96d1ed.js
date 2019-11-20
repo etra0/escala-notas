1103886f23c463b001726269aeb26c30670418ef
Sebastián Aedo <sebastian.aedo29@gmail.com>
2019-11-20 11:25:33 -0300

    Updates

---
+++ precache-manifest.d55c1897417b4c7b6cad2379fb96d1ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07480f14d9a9fd68dfd8f49bceb75835",
+    "url": "/escala-notas/index.html"
+  },
+  {
+    "revision": "5eba0576447df7b2d4af",
+    "url": "/escala-notas/static/css/main.4f75c9d2.chunk.css"
+  },
+  {
+    "revision": "4876c8b1d8b9dc671494",
+    "url": "/escala-notas/static/js/2.678a344e.chunk.js"
+  },
+  {
+    "revision": "5eba0576447df7b2d4af",
+    "url": "/escala-notas/static/js/main.c414bfd5.chunk.js"
+  },
+  {
+    "revision": "00080cd9d9492986818d",
+    "url": "/escala-notas/static/js/runtime-main.abbb98c5.js"
+  }
+]);
