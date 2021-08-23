09a7131bc9e77b74915f0ed9394564a027b489db
Sebastián Aedo <sebastian.aedo@sansano.usm.cl>
2021-08-23 14:41:18 -0400

    Updates

---
+++ precache-manifest.c9458cbd0eefcff3562dfa492ecd4eed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d3ea12a9b45408be1cd7fdd9834dca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "309ed2a0000c3c8f1a4f",
+    "url": "/static/css/main.d5010f65.chunk.css"
+  },
+  {
+    "revision": "37956c50cd2acd0f95c7",
+    "url": "/static/js/2.042d4657.chunk.js"
+  },
+  {
+    "revision": "309ed2a0000c3c8f1a4f",
+    "url": "/static/js/main.9c83a0f7.chunk.js"
+  },
+  {
+    "revision": "ee6e3a5dfd733299f8fa",
+    "url": "/static/js/runtime-main.90f971e5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/escala-notas/precache-manifest.a3b0ec8f1cbd03aa02625db98f4eb0f0.js"
+  "/precache-manifest.c9458cbd0eefcff3562dfa492ecd4eed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/escala-notas/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
